3b35293ccb1072f1e07d9be1fec348d338402722
WebbingStudio <mail@webbingstudio.com>
2018-04-23 06:15:31 +0900

    メールフォームの修正（確認画面へ移動できない）

--- dist/theme/ratio_3_2/Elements/mail_form.php
+++ dist/theme/ratio_3_2/Elements/mail_form.php
@@ -5,6 +5,9 @@
  * 
  */
 
+// ブラウザのヒストリーバック（戻るボタン）対応
+$this->Mail->token();
+
 $prefix = '';
 if (Configure::read('BcRequest.agent')) {
 	$prefix = '/' . Configure::read('BcRequest.agentAlias');
@@ -22,25 +25,27 @@ $(function(){
 </script>
 
 <?php if (!$freezed): ?>
-	<?php echo $this->Mailform->create('Message', array('class' => 'form-horizontal', 'url' => $prefix . '/' . $mailContent['MailContent']['name'] . '/confirm', 'type' => 'file')) ?>
+	<?php echo $this->Mailform->create('MailMessage', array('class' => 'form-horizontal'), array('url' => $this->BcBaser->getContentsUrl(null, false, null, false) . 'confirm', 'type' => 'file')) ?>
 <?php else: ?>
-	<?php echo $this->Mailform->create('Message', array('class' => 'form-horizontal', 'url' => $prefix . '/' . $mailContent['MailContent']['name'] . '/submit')) ?>
+	<?php echo $this->Mailform->create('MailMessage', array('class' => 'form-horizontal'), array('url' => $this->BcBaser->getContentsUrl(null, false, null, false)  . 'submit')) ?>
 <?php endif; ?>
 
-<?php echo $this->Mailform->hidden('Message.mode') ?>
+<?php $this->Mailform->unlockField('MailMessage.mode') ?>
+<?php echo $this->Mailform->hidden('MailMessage.mode') ?>
 
 <?php $this->BcBaser->element('mail_input', array('blockStart' => 1)) ?>
 
 <?php if ($mailContent['MailContent']['auth_captcha']): ?>
 	<?php if (!$freezed): ?>
 		<div class="auth-captcha clearfix">
-			<?php $this->BcBaser->img($prefix . '/' . $mailContent['MailContent']['name'] . '/captcha', array('alt' => '認証画像', 'class' => 'auth-captcha-image') ); ?>
-			<?php echo $this->Mailform->text('Message.auth_captcha', array( 'class' => 'form-control form-control-sm' ) ) ?><br>
-			&nbsp;画像の文字を入力してください<br clear="all">
-			<?php echo $this->Mailform->error( 'Message.auth_captcha', '画像と入力された文字が一致しません。もう一度入力してください。', array( 'class' => 'alert alert-danger') ); ?>
+			<?php echo $this->Mailform->authCaptcha('MailMessage.auth_captcha') ?>
+			<br />
+			&nbsp;<?php echo __('画像の文字を入力してください') ?><br clear="all" />
+			<?php echo $this->Mailform->error('MailMessage.auth_captcha', __('入力された文字が間違っています。入力をやり直してください。'), array( 'class' => 'alert alert-danger')); ?>
 		</div>
 	<?php else: ?>
-		<?php echo $this->Mailform->hidden('Message.auth_captcha') ?>
+		<?php echo $this->Mailform->hidden('MailMessage.auth_captcha') ?>
+		<?php echo $this->Mailform->hidden('MailMessage.captcha_id') ?>
 	<?php endif ?> 
 <?php endif ?>
 
@@ -65,7 +70,7 @@ $(function(){
 	<?php if ($freezed): ?>
 		<?php echo $this->Mailform->submit('書き直す', array('div' => false, 'class' => 'btn btn-default form-submit', 'id' => 'BtnMessageBack')) ?>
 		<?php echo $this->Mailform->submit('送信する', array('div' => false, 'class' => 'btn btn-lg btn-primary form-submit', 'id' => 'BtnMessageSubmit')) ?> 
-	<?php elseif ($this->action != 'submit'): ?>
+	<?php else: ?>
 		<?php echo $this->Mailform->submit('入力内容を確認', array('div' => false, 'class' => 'btn btn-lg btn-primary form-submit', 'id' => 'BtnMessageConfirm')) ?>
 	<?php endif; ?>
 <!-- /.submit --></div>
